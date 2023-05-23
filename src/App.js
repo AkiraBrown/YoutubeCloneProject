249c0616f1444f662442adb49ab96d9e44d62bd9
Akira Watson-Brown <akirawatson-brown@new-host-8.home>
2023-05-23 11:38:44 -0400

    ConfigCommit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const testYoutubeAPI = () => {
+    //console.log(process.env.REACT_APP_DEVELOPMENT_API_KEY);
+  };
+  useEffect(() => {
+    testYoutubeAPI();
+  });
+
   return <div className="App"></div>;
 }
 
