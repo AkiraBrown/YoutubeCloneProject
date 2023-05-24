aad2d733b0934558f01fe7eb25b7ddc89e67b38e
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-05-24 11:46:56 -0400

    added data to searchbar component

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,1 +1,39 @@
-export default function SearchBar() {}
+import { useState } from "react";
+import React from "react";
+import { fetchApi } from "../api/fetch.js";
+
+export default function SearchBar({
+  setSearchResults,
+  setHasSearched,
+  hasSearched,
+}) {
+  const [input, setUserInput] = useState("");
+
+  function handleUserInput(event) {
+    event.preventDefault();
+    getUserSearch(input).then((response) => {
+      setSearchResults(response.items);
+      if (!hasSearched) {
+        setHasSearched(true);
+      }
+    });
+  }
+  return (
+    <main>
+      <form onSubmit={handleUserInput}>
+        <input
+          id="input"
+          type="text"
+          placeholder="search"
+          value={input}
+          onChange={(event) => {
+            setUserInput(event.target.value);
+          }}
+        />
+        <button className="submit" type="submit">
+          Search
+        </button>
+      </form>
+    </main>
+  );
+}
