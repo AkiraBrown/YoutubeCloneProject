1e72f28b9e3895c6de225b3867c4b19cb2d67d5c
Akira Watson-Brown <akirawatson-brown@Akiras-Air.home>
2023-05-25 17:12:15 -0400

    Updated HelperFunction and Created new Components

---
+++ src/Components/Channel.js
@@ -0,0 +1,3 @@
+import React, { useEffect, useState } from "react";
+import fetchApi from "./api/fetch";
+export default function Channel() {}
--- src/Components/api/fetch.js
+++ src/Components/api/fetch.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 async function fetchApi(data) {
   let key = `key=${process.env.REACT_APP_DEVELOPMENT_API_KEY}`;
+  let resource = data.resource ? data.resource : "";
   let configObj = {
-    url: process.env.REACT_APP_DEVELOPMENT_URL + "&" + key,
+    url: `${process.env.REACT_APP_DEVELOPMENT_URL}&${key}${resource}`, //process.env.REACT_APP_DEVELOPMENT_URL + "&" + key,
     ...data,
   };
   //console.log(configObj);
