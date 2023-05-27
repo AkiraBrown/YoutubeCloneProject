74ecdfb8a470d38e15b5566ab1b49d896e358705
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-05-26 20:09:20 -0400

    error  message

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,38 +2,3 @@ import { useState } from "react";
 import React from "react";
 import { fetchApi } from "./api/fetch.js";
 
-export default function SearchBar({
-  setSearchResults,
-  setHasSearched,
-  hasSearched,
-}) {
-  const [input, setUserInput] = useState("");
-
-  function handleUserInput(event) {
-    event.preventDefault();
-    fetchApi(input).then((response) => {
-      setSearchResults(response.items);
-      if (!hasSearched) {
-        setHasSearched(true);
-      }
-    });
-  }
-  return (
-    <main>
-      <form onSubmit={handleUserInput}>
-        <input
-          id="input"
-          type="text"
-          placeholder="search"
-          value={input}
-          onChange={(event) => {
-            setUserInput(event.target.value);
-          }}
-        />
-        <button className="submit" type="submit">
-          Search
-        </button>
-      </form>
-    </main>
-  );
-}
