7d1b19e8ffdb6b17f2ff1efbf8ec98708cdd3052
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-05-26 20:51:05 -0400

    added info to about component

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,24 @@
+import React from "react";
+
 export default function About() {
+  const projectBio =
+    "This project was built by a team of 2 developers. We used a React framework to create a Youtube clone that allows the user to search & display a single video as well as the ability to share & send any video. ";
+
+  const shaqFredBio = "Shaquala's Bio";
+
+  const AkiraBrownBio = "Akira's Bio";
+
   return (
-    <div>
-      <p>About Page</p>
+    <div style={{ textAlignVertical: "center", textAlign: "center" }}>
+      <article key="about">{}</article>
+      <h2>About our Project</h2>
+      <p>{projectBio}</p>
+      <h2>About the Developers</h2>
+      <p>{shaqFredBio}</p>
+
+      <a href={"https://github.com/shaqfred"}>Shaquala's GitHub Link</a>
+
+      <p>{AkiraBrownBio}</p>
     </div>
   );
 }
